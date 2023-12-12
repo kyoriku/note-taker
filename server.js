7b26de3f81490bb5354837a9984c4b9411c92b42
Austin Graham <austiingraham@gmail.com>
2023-12-12 00:11:47 -0500

    import custom route handlers

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
