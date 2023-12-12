4c8b8b678f1c6ff445d087afb81bc2ca75df55d9
Austin Graham <austiingraham@gmail.com>
2023-12-12 01:08:35 -0500

    set up routes for API and HTML

--- server.js
+++ server.js
@@ -9,3 +9,6 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use('/api/notes', apiRoutes);
+app.use('/', htmlRoutes);
