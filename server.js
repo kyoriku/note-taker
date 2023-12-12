d4c246973d9e6c7afd36e76897e80a6795a7998c
Austin Graham <austiingraham@gmail.com>
2023-12-12 00:39:25 -0500

    set up middleware for parsing and static files

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
