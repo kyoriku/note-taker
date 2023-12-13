d52db17aa3270f6a6a305764577b3483a3190233
Austin Graham <austiingraham@gmail.com>
2023-12-13 00:59:46 -0500

    added GET endpoint to serve notes.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,6 @@
 const htmlRouter = require('express').Router();
 const path = require('path');
+
+htmlRouter.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
