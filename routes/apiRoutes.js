ab38b2b50223da84151348657a9458e0d41c2291
Austin Graham <austiingraham@gmail.com>
2023-12-14 00:35:00 -0500

    added GET endpoint to retrieve all notes, changed PORT to 3001

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,8 @@
 const apiRouter = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+
+apiRouter.get('/', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  res.json(notes);
+});
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
