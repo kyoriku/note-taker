d892a2fd1d6d874e091bff489d753c35d1e786a1
Austin Graham <austiingraham@gmail.com>
2023-12-14 02:16:22 -0500

    added POST endpoint to add a new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,3 +6,12 @@ apiRouter.get('/', (req, res) => {
   const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
   res.json(notes);
 });
+
+apiRouter.post('/', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  notes.push(newNote);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(newNote);
+});
